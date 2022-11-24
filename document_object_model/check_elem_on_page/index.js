4dfb20c187fb284f0d0a800155e378b4264c1822
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2022-11-24 20:26:56 +0200

    add

--- document_object_model/check_elem_on_page/index.js
+++ document_object_model/check_elem_on_page/index.js
@@ -1,4 +1,12 @@
-function getTitleElement{
-    const titleElem = document.querySelector('.title');
-    console.dir(titleElem)
+function getTitleElement() {
+  const titleElem = document.querySelector('.title');
+  console.dir(titleElem);
+  return titleElem;
 }
+
+function getInputElement() {
+  const inputElem = document.querySelector("input [type = 'text']");
+  console.dir = inputElem;
+  return inputElem;
+}
+getInputElement();
