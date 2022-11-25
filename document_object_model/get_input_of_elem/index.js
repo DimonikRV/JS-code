147db1070d2830ccecbc2f1d6695ff47d60123ea
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2022-11-25 13:03:56 +0200

    add

---
+++ document_object_model/get_input_of_elem/index.js
@@ -0,0 +1,19 @@
+export function getTitle() {
+  const titleText = document.querySelector('.title');
+  return titleText.textContent();
+}
+
+export function getDescription() {
+  const innerText = document.querySelector('.about');
+  return innerText.innerText();
+}
+
+export function getPlans() {
+  const innerText = document.querySelector('.plans');
+  return innerText.innerHTML();
+}
+
+export function getGoal() {
+  const innerText = document.querySelector('.goal');
+  return innerText.outerHTML();
+}
