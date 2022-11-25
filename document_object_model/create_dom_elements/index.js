d212dd9c70afeba6e2ae2ed8f8a0f153e20d3ca5
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2022-11-25 17:25:00 +0200

    add

--- document_object_model/create_dom_elements/index.js
+++ document_object_model/create_dom_elements/index.js
@@ -1,4 +1,4 @@
-function createButton(buttonText) {
+export function createButton(buttonText) {
   const bodyElem = document.querySelector('body');
   const buttonElem = document.createElement('button');
   buttonElem.textContent = buttonText;
