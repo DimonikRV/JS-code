a7f1e089f3c590734939fb3a40fac6dad2e1e595
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2022-11-26 15:08:28 +0200

    add

--- document_object_model/find_nearest_parent/index.js
+++ document_object_model/find_nearest_parent/index.js
@@ -2,5 +2,5 @@ export function getSection(num) {
   const spanElems = [...document.querySelectorAll('span')];
   const requiredNum = spanElems.find(elem => Number(elem.dataset.number) === num);
   const requiredParent = requiredNum.closest('.box');
-  return requiredParent;
+  return requiredParent.dataset.section;
 }
