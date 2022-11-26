ae75e195861dfde9d040c89afbdda0be8dc5fd9a
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2022-11-26 14:09:49 +0200

    ADD

--- document_object_model/data_attributes/index.js
+++ document_object_model/data_attributes/index.js
@@ -2,7 +2,8 @@ export function squaredNumbers() {
   [...document.querySelectorAll('.number')].map(el => {
     const num = el.dataset;
     const squaredNum = num.number ** 2;
-    el.dataset.squarednumber = squaredNum;
+    el.dataset.squaredNumber = squaredNum;
     return el;
   });
 }
+squaredNumbers();
