3db9139d8f611988928bc3e33086f9a39db8eb26
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2022-11-29 19:10:05 +0200

    add

---
+++ lesson_dom_events/search_field/index.js
@@ -0,0 +1,3 @@
+const searchBtnElem = document.querySelector('.search__btn');
+const searchInputElem = document.querySelector('.search__input');
+searchBtnElem.addEventListener('click', () => console.log(searchInputElem.value));
