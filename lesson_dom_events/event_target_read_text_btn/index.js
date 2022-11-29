b66fbe8a7964c8e1c8d98bfbb842f607b19c6286
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2022-11-29 20:14:23 +0200

    add

--- lesson_dom_events/event_target_read_text_btn/index.js
+++ lesson_dom_events/event_target_read_text_btn/index.js
@@ -1,17 +1,17 @@
-// const btnElems = document.querySelectorAll('.btn');
-// function handleClick(event) {
-//   console.log(event.target.textContent);
-// }
-// btnElems.forEach(btn => btn.addEventListener('click', handleClick));
-
-const bodyElem = document.querySelector('body');
-
+const btnElems = document.querySelectorAll('.btn');
 function handleClick(event) {
   console.log(event.target.textContent);
 }
+btnElems.forEach(btn => btn.addEventListener('click', handleClick));
 
-bodyElem.addEventListener('click', function (event) {
-  if (event.target.closest('.btn')) {
-    handleClick(event);
-  }
-});
+// const bodyElem = document.querySelector('body');
+
+// function handleClick(event) {
+//   console.log(event.target.textContent);
+// }
+
+// bodyElem.addEventListener('click', function (event) {
+//   if (event.target.closest('.btn')) {
+//     handleClick(event);
+//   }
+// });
