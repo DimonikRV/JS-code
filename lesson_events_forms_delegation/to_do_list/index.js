424b6fec5dae59ab298515bd089fb4599afb4339
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2022-12-03 13:08:33 +0200

    add

--- lesson_events_forms_delegation/to_do_list/index.js
+++ lesson_events_forms_delegation/to_do_list/index.js
@@ -1,15 +1,12 @@
 const tasks = [
-  { text: 'Buy milk', done: false, id: 1, },
-  { text: 'Pick up Tom from airport', done: false, id: 2, },
-  { text: 'Visit party', done: false, id: 3,},
-  { text: 'Visit doctor', done: true, id: 4,},
-  { text: 'Buy meat', done: true, id: 5,},
+  { text: 'Buy milk', done: false, id: 1 },
+  { text: 'Pick up Tom from airport', done: false, id: 2 },
+  { text: 'Visit party', done: false, id: 3 },
+  { text: 'Visit doctor', done: true, id: 4 },
+  { text: 'Buy meat', done: true, id: 5 },
 ];
 
 const listElem = document.querySelector('.list');
-
-
-
 
 const renderTasks = tasksList => {
   const tasksElems = tasksList
@@ -21,7 +18,7 @@ const renderTasks = tasksList => {
       checkbox.setAttribute('type', 'checkbox');
       checkbox.checked = done;
       checkbox.classList.add('list__item-checkbox');
-      checkbox.dataset.id = id; 
+      checkbox.dataset.id = id;
       if (done) {
         listItemElem.classList.add('list__item_done');
       }
@@ -32,46 +29,43 @@ const renderTasks = tasksList => {
 
   listElem.append(...tasksElems);
 };
- renderTasks(tasks);
+renderTasks(tasks);
 
 const checkboxElem = document.querySelector('.list__item-checkbox');
 
-const changeDone = (event) =>{
-  const isCheckbox = event.target.classList.contains('list__item-checkbox')
-  if(!isCheckbox) return;
+const changeDone = event => {
+  const isCheckbox = event.target.classList.contains('list__item-checkbox');
+  if (!isCheckbox) return;
 
   const checkedEl = Number(event.target.dataset.id);
   // console.log(checkedEl)
-  tasks.forEach((task) => {
-    if(task.id === checkedEl){
-      
-    task.done = task.done === true?false: true
-   
-  }
-  listElem.innerHTML = '';
-})
- renderTasks(tasks)  
-} 
- 
+  tasks.forEach(task => {
+    if (task.id === checkedEl) {
+      task.done = task.done === true ? false : true;
+    }
+    listElem.innerHTML = '';
+  });
+  renderTasks(tasks);
+};
+
 listElem.addEventListener('change', changeDone);
 
 const createButtonElem = document.querySelector('.create-task-btn');
 
-
 // const inputElem = document.querySelector('.task-input');
 
-
-const createTask = () =>{
+const createTask = () => {
   const inputElem = document.querySelector('.task-input');
-  const text = inputElem.value
-  const newTask = { text: text, done: false, id: Math.round(Math.random() * (800 - 6) + 6), };
+  const text = inputElem.value;
+  if (!text) return;
+  const newTask = { text: text, done: false, id: Math.round(Math.random() * (800 - 6) + 6) };
   tasks.unshift(newTask);
   listElem.innerHTML = '';
-    renderTasks(tasks);
-    inputElem.value = '';
-}
+  renderTasks(tasks);
+  inputElem.value = '';
+};
 
-createButtonElem.addEventListener('click', createTask)
+createButtonElem.addEventListener('click', createTask);
 
 // setNewTask = (event) =>{
 //   createTask()
@@ -80,12 +74,10 @@ createButtonElem.addEventListener('click', createTask)
 // }
 // createButtonElem.addEventListener('click', setNewTask)
 
-console.log(tasks)
-
+console.log(tasks);
 
 // const createElem = event =>{
 //   const createButton = event.target
-  
 
 // }
 // createButtonElem.addEventListener('click', createElem)
