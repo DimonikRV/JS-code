e0e9cda4191940d21399cecd10c8ebf6b3cc8c3d
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2022-12-06 12:56:38 +0200

    add

--- lesson_work_with_date/adjustment_date_components/index.js
+++ lesson_work_with_date/adjustment_date_components/index.js
@@ -1,6 +1,7 @@
-export const dayOfWeek = (date, days) => {
-  const daysOfWeek = [Su, Mo, Tu, We, Th, Fr, St];
+const dayOfWeek = (date, days) => {
+  const daysOfWeek = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
   const day = new Date(date).getDate();
   const dayInFuture = new Date(date).setDate(day + days);
   return daysOfWeek[new Date(dayInFuture).getDay()];
 };
+console.log(dayOfWeek(new Date(2019, 0, 1), 14));
