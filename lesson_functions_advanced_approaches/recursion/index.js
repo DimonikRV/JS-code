eb64f25d5b24d8307e765088eb0acd04e2d36d63
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2022-12-12 17:10:23 +0200

    add

---
+++ lesson_functions_advanced_approaches/recursion/index.js
@@ -0,0 +1,4 @@
+const markFavorites = (tree, markFavorites) => {
+  const isFavorite = markFavorites.includes(tree.id);
+  return;
+};
--- lesson_work_with_date/sort_students/index.js
+++ lesson_work_with_date/sort_students/index.js
@@ -14,25 +14,25 @@ const studentsBirthDays = students => {
     'Nov',
     'Dec',
   ];
-  const studentsBday = students.map(student => {
+
+  const studentsBday = students.reduce((acc, student) => {
     const monthOfBorn = month[new Date(student.birthDate).getMonth()];
-    return { ...student, birthDate: monthOfBorn };
-  });
+    return {
+      ...acc,
+      [monthOfBorn]: [],
+    };
+  }, {});
   console.log(studentsBday);
-
-  studentsBday.forEach(student => {
-    sortedObj.hasOwnProperty(student.birthDate)
-      ? (sortedObj[student.birthDate] = [student.name].concat(student.name))
-      : (sortedObj[student.birthDate] = [student.name]);
-  });
-  return sortedObj;
+// if(month.includes(studentsBday[]))
+  
 };
-
 console.log(
   studentsBirthDays([
     { name: 'Tom', birthDate: '01/15/2010' },
     { name: 'Ben', birthDate: '01/17/2008' },
     { name: 'Sam', birthDate: '03/05/2005' },
+    { name: 'Bob', birthDate: '02/05/2007' },
+    { name: 'Ann', birthDate: '02/08/2007' },
   ]),
 );
 //  => {Jan: ['Tom','Ben'], Mar: ['Sam']}
