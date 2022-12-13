98fba77c8f7611271bff23e53f847973e23c1ef3
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2022-12-13 13:05:49 +0200

    add

--- lesson_functions_advanced_approaches/recursion/index.js
+++ lesson_functions_advanced_approaches/recursion/index.js
@@ -3,6 +3,6 @@ export const markFavorites = (tree, favorites) => {
   return {
     ...tree,
     isFavorite,
-    node: tree.node.map(childNode => markFavorites(childNode, markFavorites())),
+    node: tree.node.map(childNode => markFavorites(childNode, favorites)),
   };
 };
