0ecf2b6796171d770c7b4a30c18756f9828f91f5
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2022-12-15 18:46:47 +0200

    add

--- lesson_asynchronous_code/write_with_interval/index.js
+++ lesson_asynchronous_code/write_with_interval/index.js
@@ -5,13 +5,20 @@
  * @param {number} period
  * @return {undefined}
  */
-export const pinger = (count, period) => {
+const pinger = (count, period) => {
   const pingTxt = 'Ping';
   console.log(pingTxt);
-  const interval = setInterval(() => console.log(pingTxt), period);
-  let item = count;
+  let item = count - 1;
+  const interval = setInterval(() => {
+    if (item > 0) {
+      item -= 1;
+      console.log(pingTxt);
+    } else {
+      clearInterval(interval);
+    }
+  }, period);
 };
 
 // examples
-pinger(5, 100); // makes 5 writes with 100 ms interval
+// pinger(5, 100); // makes 5 writes with 100 ms interval
 pinger(7, 150); // makes 7 writes with 1500 ms interval
