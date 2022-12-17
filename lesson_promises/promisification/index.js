2b7a3dc470ee5498e287bf46d21834cbdc9b2347
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2022-12-17 13:55:47 +0200

    add

--- lesson_promises/promisification/index.js
+++ lesson_promises/promisification/index.js
@@ -1,9 +1,24 @@
-const addImageV2 = url => {
+export const addImageV2 = url => {
   const addImage = new Promise((resolve, reject) => {
-  const img = document.createElement('img');
-  img.setAttribute('alt', 'User avatar');
-  img.src = url;
-});
+    const img = document.createElement('img');
+    img.setAttribute('alt', 'User avatar');
+    img.src = url;
+
+    const pageElem = document.querySelector('.page');
+    pageElem.append(img);
+
+    const onImageLoaded = () => {
+      const { width, height } = img;
+      resolve({ width, height });
+    };
+
+    const onImageLoadError = () => reject(new Error('Image load failed'));
+
+    img.addEventListener('load', onImageLoaded);
+
+    img.addEventListener('error', onImageLoadError);
+  });
+};
 
 // examples
 
