4d3bdb88b25c9413564f203bdd28da7799958fcf
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2022-12-18 13:57:56 +0200

    add

---
+++ lesson_promise_handlers_chaining/fix_request_on_serer/index.js
@@ -0,0 +1,30 @@
+const serverResponsePromise = () =>
+  new Promise(resolve => {
+    const serverResponse = {
+      ok: true,
+      json() {
+        return Promise.resolve({
+          name: 'John',
+          age: 20,
+        });
+      },
+    };
+    resolve(serverResponse.json());
+  });
+
+/*
+ * допиши первый обработчик, чтобы во второй попал объект пользователя
+ */
+
+serverResponsePromise()
+  .then(response => {
+    const { name, age } = response;
+    return { name, age };
+  })
+  .then(result => {
+    console.log(result); // { name: 'John', age: 20 }
+  });
+
+console.log(
+  '!!! Обрати внимание, что этот текст вывелся самым первым. Ведь это синхронный код, а промисы - асинхронны !!!',
+);
