cd360ade838995772b23dac1eafaf43d86105758
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2022-12-18 11:15:05 +0200

    add

---
+++ lesson_promise_handlers_chaining/promise_hendlers/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @param {string} userId
+ * @return {promise}
+ */
+export const requestUserData = userId => {
+  const request = new Promise((resolve, reject) => {
+    if (userId === 'broken') {
+      setTimeout(() => reject(new Error('User not found')), 500);
+    }
+    setTimeout(
+      () =>
+        resolve({
+          name: 'John',
+          age: 17,
+          userId,
+          email: `${userId}@example.com`,
+        }),
+      1000,
+    );
+  });
+};
