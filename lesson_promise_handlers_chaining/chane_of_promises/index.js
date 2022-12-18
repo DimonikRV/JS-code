d0a741ea70f0de7ba5c5450a611d093c2a428969
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2022-12-18 22:17:58 +0200

    add

--- lesson_promise_handlers_chaining/chane_of_promises/index.js
+++ lesson_promise_handlers_chaining/chane_of_promises/index.js
@@ -1,23 +1,22 @@
 export const asyncCalculator = numbr =>
   new Promise(resolve => {
     setTimeout(() => {
-      console.log(`Initial value:${numbr}`);
+      console.log(`Initial value: ${numbr}`);
       resolve(numbr);
     }, 500);
   })
-    .then(
-      numbr =>
-        new Promise(resolve => {
-          setTimeout(() => {
-            const squaredNum = numbr ** 2;
-            console.log(`Squared value:${squaredNum}`);
-            resolve(squaredNum);
-          }, 500);
-        }),
-    )
+    .then(numbr => {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          const squaredNum = numbr ** 2;
+          console.log(`Squared value: ${squaredNum}`);
+          resolve(squaredNum);
+        }, 500);
+      });
+    })
     .then(numbr => {
       const result = numbr * 2;
-      console.log(`Doubled value:${result}`);
+      console.log(`Doubled value: ${result}`);
       return result;
     });
 
