ea6e642920ce02804188b5cfa70573182e0e2691
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2022-12-19 16:55:06 +0200

    add

--- lesson_promise_static_methods/static_methods/index.js
+++ lesson_promise_static_methods/static_methods/index.js
@@ -2,7 +2,7 @@ const getOneValue = values => {
   values.filter(notIsNaN => !isNaN(notIsNaN)).reduce((acc, values) => acc + Number(values), 0);
 };
 
-const asyncSum = (...asyncNumber) =>
+export const asyncSum = (...asyncNumber) =>
   Promise.all(asyncNumber)
     .then(numbers => getOneValue(numbers))
     .catch(() => Promise.reject(new Error("Can't calculate")));
