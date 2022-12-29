7b13f99b12719b38e06bdda2cca2b3e20aba09c4
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2022-12-29 12:49:56 +0200

    add

--- lesson_error_handling/github_repository_list/src/scripts/index.js
+++ lesson_error_handling/github_repository_list/src/scripts/index.js
@@ -5,6 +5,7 @@ import { toggleSpiner } from './spiner.js';
 
 const formBtnElem = document.querySelector('.name-form__btn');
 const inputElem = document.querySelector('.name-form__input');
+
 const defaultUser = {
   avatar_url: 'https://avatars3.githubusercontent.com/u10001',
   name: '',
@@ -15,15 +16,17 @@ renderUser(defaultUser);
 
 const onClickRequest = () => {
   cleareList();
+  toggleSpiner();
   const userName = inputElem.value;
   getRequest(userName)
     .then(userData => {
       renderUser(userData);
-      toggleSpiner();
       return userData.repos_url;
     })
     .then(url => fetchReposList(url))
-    .then(reposList => renderReposList(reposList))
+    .then(reposList => {
+      renderReposList(reposList);
+    })
     .catch(error => alert(error.message))
     .finally(() => toggleSpiner());
 };
