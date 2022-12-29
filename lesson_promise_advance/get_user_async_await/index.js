cbb65bbd4a8bf101a7e93952ab5ef46f328542d3
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2022-12-29 19:45:49 +0200

    add

--- lesson_promise_advance/get_user_async_await/index.js
+++ lesson_promise_advance/get_user_async_await/index.js
@@ -2,12 +2,13 @@
 
 export const fetchUser = async userId => {
   const response = await fetch(`https://api.github.com/users/${userId}`);
-  if (response.status === 404) {
-    return null;
-  }
   if (!response.ok) {
     throw new Error('Failed to get user data');
   }
+  if (response.status === 404) {
+    return null;
+  }
+
   const userData = await response.json();
   return userData;
 };
