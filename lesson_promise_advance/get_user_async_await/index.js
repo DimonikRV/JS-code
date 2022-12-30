dcd0ce5a526a225732381547f6e011cbb123ef54
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2022-12-30 11:16:13 +0200

    add

--- lesson_promise_advance/get_user_async_await/index.js
+++ lesson_promise_advance/get_user_async_await/index.js
@@ -1,13 +1,14 @@
 'use strict';
 
 export const fetchUser = async userId => {
-  const response = await fetch(`https://api.github.com/users/${userId}`);
-  if (response.status === 404) {
-    return null;
+  try {
+    const response = await fetch(`https://api.github.com/users/${userId}`);
+    if (response.status === 404) {
+      return null;
+    }
+    const userData = await response.json();
+    return userData;
+  } catch (error) {
+    throw new Error('Failed to get user data');
   }
-  if (!response.ok) {
-    return new Promise.reject(new Error('Failed to get user data'));
-  }
-  const userData = await response.json();
-  return userData;
 };
--- lesson_promise_advance/user_list_github/src/scripts/gateways.js
+++ lesson_promise_advance/user_list_github/src/scripts/gateways.js
@@ -1,5 +1,5 @@
 export const getRequest = async userName => {
-  const response = fetch(`https://api.github.com/users/${userName}`);
+  const response = await fetch(`https://api.github.com/users/${userName}`);
   if (!response.ok) {
     throw new Error('Failed to load data');
   }
@@ -12,6 +12,6 @@ export const fetchReposList = async url => {
   if (!response.ok) {
     throw new Error('Failed to load data');
   }
-  const reposList = await response.json();
-  return reposList;
+  const repoData = await response.json();
+  return repoData;
 };
